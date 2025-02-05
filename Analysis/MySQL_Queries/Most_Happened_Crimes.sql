b4e77545ac2ec5a989ae149d19f41d2235d67cf7
MalakaSupun <malakag@sltc.ac.lk>
2025-02-05 23:27:35 +0530

    Add Saved MySQL Queries

---
+++ Analysis/MySQL_Queries/Most_Happened_Crimes.sql
@@ -0,0 +1,10 @@
+USE LAPD_Crime_Data;
+SELECT 
+     Crm_Cd_Desc AS Crime_Type,
+     COUNT(*) AS CrimeCount
+FROM 
+	crime_data
+GROUP BY Crime_type
+HAVING CrimeCount >= "1000"
+ORDER BY CrimeCount DESC
+LIMIT 100
